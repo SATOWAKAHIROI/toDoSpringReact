a9ae06098136738bec3a9e374534321d21e738b1
hiroto saikawa <satowakahiroi@hirotonoMacBook-Air.local>
2025-12-09 17:08:28 +0900

    TodoServiceを作成

--- backend/src/main/java/com/example/backend/repository/TodoRepository.java
+++ backend/src/main/java/com/example/backend/repository/TodoRepository.java
@@ -2,19 +2,25 @@ package com.example.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.backend.entity.Todo;
-import com.example.backend.entity.User;
 
 import java.util.List;
-
+import java.util.Optional;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     /**
      * 指定したユーザーが登録したTodoの一覧取得
+     * 
      * @param createdBy ユーザー
      * @return Todoのリスト
      */
-    List<Todo> findByCreatedBy(User createdBy);
+    List<Todo> findByCreatedById(Long userId);
+
+    Optional<Todo> findByIdAndCreatedById(Long toDoId, Long userId);
+
+    @Transactional
+    void deleteByIdAndCreatedById(Long toDoId, Long userId);
 }
