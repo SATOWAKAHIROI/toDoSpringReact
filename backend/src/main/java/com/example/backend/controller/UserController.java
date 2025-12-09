a6d04b4eddaa08536872163bea6b20cbb97b8195
satowakahiroi <hitobussi@gmail.com>
2025-12-09 14:09:04 +0900

    UserControllerとTodoControllerを作成、AuthResponseを作成

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,9 +1,25 @@
 package com.example.backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.backend.dto.common.ApiResponse;
+import com.example.backend.dto.request.LoginRequest;
+import com.example.backend.dto.request.UserRequest;
+import com.example.backend.dto.response.AuthResponse;
+import com.example.backend.dto.response.UserResponse;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 
 
@@ -12,8 +28,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController{
     @GetMapping("/login")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    public ResponseEntity<ApiResponse<AuthResponse>> login(@Valid @RequestBody LoginRequest loginRequest) {
+        // authロジックは後ほど実装
+        AuthResponse auth = new AuthResponse(null, null);
+        return ResponseEntity.ok(ApiResponse.success(auth));
     }
-    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<UserResponse>> findById(@PathVariable Long id) {
+        // ユーザー取得するロジックは後で作成
+        UserResponse user = new UserResponse();
+        return ResponseEntity.ok(ApiResponse.success(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<UserResponse>> create(@Valid @RequestBody UserRequest userRequest) {
+        // ユーザー作成ロジックは後で作成
+        UserResponse user = new UserResponse();
+        return ResponseEntity.ok(ApiResponse.success(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<UserResponse>> edit(@PathVariable Long id, @Valid @RequestBody UserRequest userRequest) {
+        // ユーザー編集ロジックは後で作成
+        UserResponse user = new UserResponse();
+        return ResponseEntity.ok(ApiResponse.success(user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long id){
+        // ユーザー削除ロジックは後で作成
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
 }
